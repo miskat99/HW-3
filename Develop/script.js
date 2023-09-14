54212bd7a964be0d04484e81ec6db18306e472a5
Abdullah Miskat <miskatabdullah9722@gmail.com>
2023-09-14 14:27:23 -0400

    add the hw3

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,32 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+function generatePass() {
+  let pass = '';
+  let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+      'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+  for (let i = 1; i <= 8; i++) {
+      let char = Math.floor(Math.random()
+          * str.length + 1);
+
+      pass += str.charAt(char)
+  }
+
+  return pass;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(generatePass());
