4056164318aa8fa20996256c033ee128b9b98fb5
Abdullah Miskat <miskatabdullah9722@gmail.com>
2023-09-14 14:40:01 -0400

    add script

--- assets/script.js
+++ assets/script.js
@@ -30,3 +30,21 @@ function generatePass() {
 }
 
 console.log(generatePass());
+
+
+function generatePass() {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+ 
+    for (let i = 1; i <= 8; i++) {
+        let char = Math.floor(Math.random()
+            * str.length + 1);
+ 
+        pass += str.charAt(char)
+    }
+ 
+    return pass;
+}
+ 
+console.log(generatePass());
